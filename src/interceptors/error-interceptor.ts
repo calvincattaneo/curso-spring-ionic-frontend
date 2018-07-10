8d413783f914c79aecc7c3ec9e46871193759980
Calvin Mendes Cattaneo <calvin.cattaneo@gmail.com>
2018-07-10 13:34:15 -0300

    Tratamento padrao para outros erros com Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,11 +2,12 @@ import { Injectable } from "@angular/core";
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
 import { StorageService } from "../services/storage.service";
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService) {
+    constructor(public storage: StorageService, public alerCtrl: AlertController) {
 
     }
 
@@ -25,8 +26,16 @@ export class ErrorInterceptor implements HttpInterceptor {
                 console.log(errorObj);
 
                 switch(errorObj.status) {
+                    case 401:
+                    this.handle401();
+                    break;
+
                     case 403:
                     this.handle403();
+                    break;
+
+                    default:
+                    this.handleDefaultError(errorObj);
                     break;
                 }
 
@@ -34,8 +43,32 @@ export class ErrorInterceptor implements HttpInterceptor {
         }) as any;
     }
 
+    handle401(){
+        let alert = this.alerCtrl.create({
+            title: 'Erro 401: falha de autenticação',
+            message: 'Email ou senha incorretos',
+            enableBackdropDismiss: false,
+            buttons: [
+                {text: 'OK'}
+            ]
+        });
+        alert.present();
+    }
+
     handle403() {
         this.storage.setLocalUser(null);
+    }
+
+    handleDefaultError(errorObj) {
+        let alert = this.alerCtrl.create({
+            title: 'Erro '+ errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                {text: 'OK'}
+            ]
+        });
+        alert.present();
     }
 }
 
